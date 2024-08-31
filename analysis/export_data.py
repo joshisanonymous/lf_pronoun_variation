56f762c3f7676c04398a805977e76a0227c98b14
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-08-31 10:42:47 -0400

    Initialization of ELAN export

---
+++ analysis/export_data.py
@@ -0,0 +1,8 @@
+###########################################################
+# This script accesses ELAN and exports pronoun tokens in #
+# a format that can then be imported into R for analysis. #
+###########################################################
+
+import pympi
+
+elan_files_loc = "../data/transcripts/"
