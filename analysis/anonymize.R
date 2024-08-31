454aa1effb1fefeecea858d68e28a1cc6c8e040b
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-08-31 10:42:18 -0400

    Initialization of anonymization

---
+++ analysis/anonymize.R
@@ -0,0 +1,4 @@
+
+dataDir <- "../data/"
+participantKey <- read.csv(paste(dataDir, "participant_key.csv", sep = ""), sep = "\t")
+anonNames <- read.csv(paste(dataDir, "anon_names.csv", sep = ""), sep = "\t")
