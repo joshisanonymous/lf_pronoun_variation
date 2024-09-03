05e1e59a141171f35c2eccd87863cefd8b6b81b3
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-09-03 15:03:55 -0400

    Collapsed into one ignore

--- analysis/anonymize.R
+++ analysis/anonymize.R
@@ -1,4 +1,27 @@
-
+# Variables ---------------------------------
 dataDir <- "../data/"
+
+# Data --------------------------------------
 participantKey <- read.csv(paste(dataDir, "participant_key.csv", sep = ""), sep = "\t")
 anonNames <- read.csv(paste(dataDir, "anon_names.csv", sep = ""), sep = "\t")
+
+# Functions ---------------------------------
+anonymizeNames <- function(realName) {
+  replacers <- anonNames[anonNames$Type == realName["Type"],]
+  replacers <- sample(replacers$Name)
+  for(replacer in replacers) {
+    if(replacer != realName["Real.Name"] && !(replacer %in% participantKey$Anonymized)) {
+      participantKey[participantKey$Real.Name == realName["Real.Name"] &
+                     participantKey$Type == realName["Type"],]$Anonymized <- replacer
+      break
+    }
+  }
+}
+
+# Process ------------------------------------
+# get names that haven't been anonymized
+toAnonymize <- participantKey[is.na(participantKey$Anonymized),]
+# anonymize them
+apply(toAnonymize, 1, anonymizeNames)
+apply(toAnonymize, 1, print(toAnonymize$Type))
+# save as file
