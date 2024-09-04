3e96784f0db14ac25daf93f2c0c1c176f2020b70
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2024-09-04 17:27:20 -0400

    Finished anonymize script

--- analysis/anonymize.R
+++ analysis/anonymize.R
@@ -2,26 +2,26 @@
 dataDir <- "../data/"
 
 # Data --------------------------------------
-participantKey <- read.csv(paste(dataDir, "participant_key.csv", sep = ""), sep = "\t")
-anonNames <- read.csv(paste(dataDir, "anon_names.csv", sep = ""), sep = "\t")
+participantKey <- read.csv(paste(dataDir, "participant_key.csv", sep = ""),
+                           sep = "\t", fileEncoding = "UTF-8")
+anonNames <- read.csv(paste(dataDir, "anon_names.csv", sep = ""),
+                      sep = "\t", fileEncoding = "UTF-8")
 
-# Functions ---------------------------------
-anonymizeNames <- function(realName) {
-  replacers <- anonNames[anonNames$Type == realName["Type"],]
-  replacers <- sample(replacers$Name)
-  for(replacer in replacers) {
-    if(replacer != realName["Real.Name"] && !(replacer %in% participantKey$Anonymized)) {
-      participantKey[participantKey$Real.Name == realName["Real.Name"] &
-                     participantKey$Type == realName["Type"],]$Anonymized <- replacer
-      break
+# Process ------------------------------------
+for(row in 1:nrow(participantKey)) {
+  if(is.na(participantKey[row, "Anonymized"])) {
+    replacers <- anonNames[anonNames$Type == participantKey[row, "Type"],]
+    replacers <- sample(replacers$Name)
+    for(replacer in replacers) {
+      if(!(identical(replacer, participantKey[row, "Real.Name"])) &&
+         !(replacer %in% participantKey$Anonymized)) {
+        participantKey[row, "Anonymized"] <- replacer
+        break
+      }
     }
   }
 }
 
-# Process ------------------------------------
-# get names that haven't been anonymized
-toAnonymize <- participantKey[is.na(participantKey$Anonymized),]
-# anonymize them
-apply(toAnonymize, 1, anonymizeNames)
-apply(toAnonymize, 1, print(toAnonymize$Type))
-# save as file
+# Save ---------------------------------------
+write.csv(participantKey, file = paste(dataDir, "participant_key.csv", sep = ""),
+          sep = "\t", fileEncoding = "UTF-8")
