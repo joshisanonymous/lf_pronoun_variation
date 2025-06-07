b8d4d0807152873d935477e98c3675b2e535e0aa
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2025-06-07 13:43:04 -0400

    Commented out logit models for now

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -125,23 +125,23 @@ indOccGend <- fisher.test(table(participants$Occupation, participants$Ethnicity)
 # Carmichael (2022), which suggest 1sg effects for être vs avoir and
 # verbs of opinion/belief.
 
-logitModels <- list(
-  firstSg = multinomResponse("1sg"),
-  secondSgT = binomResponse("2sg.T"),
-  # secondSgV = multinomResponse("2sg.V"), count too low to be meaningful
-  thirdSgF = binomResponse("3sg.F"),
-  thirdSgM = binomResponse("3sg.M"),
-  firstPl = multinomResponse("1pl"),
-  # secondPl = multinomResponse("2pl"), count too low to be meaningful
-  thirdPl = multinomResponse("3pl"),
-  # demostrative = multinomResponse("demo"), categorically "ça"
-  expletive = multinomResponse("expl"),
-  impersonal = multinomResponse("imp")
-)
+# logitModels <- list(
+#   firstSg = multinomResponse("1sg"),
+#   secondSgT = binomResponse("2sg.T"),
+#   # secondSgV = multinomResponse("2sg.V"), count too low to be meaningful
+#   thirdSgF = binomResponse("3sg.F"),
+#   thirdSgM = binomResponse("3sg.M"),
+#   firstPl = multinomResponse("1pl"),
+#   # secondPl = multinomResponse("2pl"), count too low to be meaningful
+#   thirdPl = multinomResponse("3pl"),
+#   # demostrative = multinomResponse("demo"), categorically "ça"
+#   expletive = multinomResponse("expl"),
+#   impersonal = multinomResponse("imp")
+# )
 
-small3plModel <- mblogit(ProUnder ~ PredType + Ethnicity,
-        data = tokens[tokens$ProType == "3pl",],
-        random = list(~ 1|Name, ~ 1|PredUnder))
+# small3plModel <- mblogit(ProUnder ~ PredType + Ethnicity,
+#         data = tokens[tokens$ProType == "3pl",],
+#         random = list(~ 1|Name, ~ 1|PredUnder))
 
 # Check for multicollinearity between factors and remove those that are
 # highly collinear. In particular, verify whether ethnicity and race
@@ -155,19 +155,20 @@ small3plModel <- mblogit(ProUnder ~ PredType + Ethnicity,
 
 # Multinomial logistic model for the relationship between frequency of French
 # use and alter type (i.e., core alters vs non-core)
-# coreByFrMultinom <- mblogit(Alter.French.Frequency ~ Alter.Type,
-#                             data = networks,
-#                             random = ~ 1|Participant)
-# 
+coreByFrMultinom <- mblogit(Alter.French.Frequency ~ Alter.Type,
+                            data = networks,
+                            random = ~ 1|Name)
+
 # # Is there a difference between the anglophone and francophone ethnic homophily?
-# homophByLanguageTtest <- t.test(participants$Anglo.Network.Ethnic.Homophily,
-#                                 participants$Franco.Network.Ethnic.Homophily,
-#                                 paired = TRUE)
-# 
+homophByLanguageTtest <- t.test(participants$Anglo.Network.Ethnic.Homophily,
+                                participants$Franco.Network.Ethnic.Homophily,
+                                paired = TRUE)
+
 # # Is one ethnic group more homophilous than the other?
-# homophByEthnicGroupTtest <- t.test(participants[participants$Ethnicity == "Creole",
-#                                                 "Network.Ethnic.Homophily"],
-#                                    participants[participants$Ethnicity == "Cajun",
-#                                                 "Network.Ethnic.Homophily"])
+homophByEthnicGroupTtest <- t.test(participants[participants$Ethnicity == "Creole",
+                                                "Network.Ethnic.Homophily"],
+                                   participants[participants$Ethnicity == "Cajun",
+                                                "Network.Ethnic.Homophily"])
+
 source("maps.R")
 source("plots-final.R")
